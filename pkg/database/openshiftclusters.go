4d5b9e70c356bc7eac44a6d3768a1a47452a642c
Angus Salkeld <asalkeld@redhat.com>
2019-12-04 15:40:23 +1000

    generate mocks for database.OpenshiftClusters

--- pkg/database/openshiftclusters.go
+++ pkg/database/openshiftclusters.go
@@ -1,5 +1,8 @@
 package database
 
+//go:generate go run ../../vendor/github.com/golang/mock/mockgen -destination=../util/mocks/mock_$GOPACKAGE/$GOPACKAGE.go github.com/jim-minter/rp/pkg/$GOPACKAGE OpenShiftClusters
+//go:generate go run ../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/jim-minter/rp -e -w ../util/mocks/mock_$GOPACKAGE/$GOPACKAGE.go
+
 import (
 	"context"
 	"fmt"
