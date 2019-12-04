18d4030c592e46bf6ced16634c228e9da1977f20
Angus Salkeld <asalkeld@redhat.com>
2019-12-04 16:29:35 +1000

    generate mocks for env/shared/dns

--- pkg/env/shared/dns/dns.go
+++ pkg/env/shared/dns/dns.go
@@ -1,5 +1,8 @@
 package dns
 
+//go:generate go run ../../../../vendor/github.com/golang/mock/mockgen -destination=../../../util/mocks/mock_env/shared/mock_$GOPACKAGE/$GOPACKAGE.go github.com/jim-minter/rp/pkg/env/shared/$GOPACKAGE Manager
+//go:generate go run ../../../../vendor/golang.org/x/tools/cmd/goimports -local=github.com/jim-minter/rp -e -w ../../../util/mocks/mock_env/shared/mock_$GOPACKAGE/$GOPACKAGE.go
+
 import (
 	"context"
 	"fmt"
